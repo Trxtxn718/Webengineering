e50b5ddb44c357cb6c0f5a2b6336597d01a3718e
Trxtxn718 <tristankopp@gmx.net>
2023-05-24 14:06:03 +0200

    Einfügen von Aufgabe 3

---
+++ Vorlesung 3/Aufgaben/Calculator/script.js
@@ -0,0 +1,17 @@
+let currentResult = '';
+
+function appendNumber(number) {
+    // to be implemented
+}
+
+function appendOperator(operator) {
+    // to be implemented
+}
+
+function calculate() {
+    // to be implemented
+}
+
+function clearResult() {
+    // to be implemented
+}
---
+++ Vorlesung 3/Aufgaben/tictactoe/script.js
@@ -0,0 +1,11 @@
+let board = ['', '', '', '', '', '', '', '', ''];
+let currentPlayer = 'X';
+let gameOver = false;
+
+function makeMove(index) {
+    // to be implemented
+}
+
+function resetGame() {
+    // to be implemented
+}
